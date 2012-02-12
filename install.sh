6a152cd6a048491d738b8295c59401435a70bff3
Mark Pitchless <markpitchless@gmail.com>
2012-02-12 20:54:41 +0000

    Fix the installer to use the right etc dir.

--- install.sh
+++ install.sh
@@ -3,12 +3,15 @@
 if [ -z "$PREFIX" ]; then
     PREFIX="/usr/local"
 fi
+if [ -z "$PITCHNAS_CONF_DIR" ]; then
+    PITCHNAS_CONF_DIR="$PREFIX/etc/pitchnas"
+fi
 
 install_opt="-v"
 
 install $install_opt -d "$PREFIX/bin"
-install $install_opt -d "$PREFIX/etc"
-install $install_opt -d "$PREFIX/etc/pre.d"
+install $install_opt -d "$PITCHNAS_CONF_DIR"
+install $install_opt -d "$PITCHNAS_CONF_DIR/pre.d"
 
 install $install_opt bin/* "$PREFIX/bin"
-install $install_opt -m 644 etc/config.sample "$PREFIX/etc/"
+install $install_opt -m 644 etc/config.sample "$PITCHNAS_CONF_DIR"
